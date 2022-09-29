dabd295d960143248362f0451720e7ab9c85cee7
Ilham Binyetov <ilhamgb@code.edu.az>
2022-09-29 16:51:53 +0400

    Create Database Connection and Fixed Startup

---
+++ IdentityManager/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using IdentityManager.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityManager.Controllers
+{
+    public class AccountController:Controller
+    {
+        public AccountController()
+        {
+
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Register()
+        {
+            RegisterViewModel registerVM = new RegisterViewModel();
+
+            return View(registerVM);
+        }
+    }
+}
