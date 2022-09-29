1027755fffb90085b0736d47e8d870b2591b4810
Ilham Binyetov <ilhamgb@code.edu.az>
2022-09-29 16:31:13 +0400

    initial commit

---
+++ IdentityManager/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using IdentityManager.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityManager.Data
+{
+    public class AppDbContext:IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }  
+    }
+}
+     
