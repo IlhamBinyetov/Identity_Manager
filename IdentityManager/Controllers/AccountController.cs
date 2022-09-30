fca0528c723a54687bcba4e6d0b9bf7389a4611a
Ilham Binyetov <ilhamgb@code.edu.az>
2022-09-30 11:14:20 +0400

    AddLogOut View and Inject userManager to Partial View

--- IdentityManager/Controllers/AccountController.cs
+++ IdentityManager/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using IdentityManager.ViewModels;
+﻿using IdentityManager.Models;
+using IdentityManager.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,11 +9,14 @@ using System.Threading.Tasks;
 
 namespace IdentityManager.Controllers
 {
-    public class AccountController:Controller
+    public class AccountController : Controller
     {
-        public AccountController()
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signManager;
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
-
+            _userManager = userManager;
+            _signManager = signInManager;
         }
 
 
@@ -27,6 +32,50 @@ namespace IdentityManager.Controllers
             RegisterViewModel registerVM = new RegisterViewModel();
 
             return View(registerVM);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel registerVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = registerVM.Email,
+                    Email = registerVM.Email,
+                    Name = registerVM.Name
+                };
+
+                var result = await _userManager.CreateAsync(user, registerVM.Password);
+                if (result.Succeeded)
+                {
+                    await _signManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+            }
+           
+            return View(registerVM);
+        }
+
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogOff()
+        {
+            await _signManager.SignOutAsync();
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
         }
     }
 }
